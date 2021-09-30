8e3bab830371c3d72469325fa92d2f3073b0e977
82102 <sms7624@gmail.com>
2021-09-30 23:39:14 +0900

    Insert, Delete

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -2,14 +2,25 @@ import React, { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
-    const [value, setValue] = useState('11');
+const TodoInsert = ({ onInsert }) => {
+    const [value, setValue] = useState('');
 
     const onChange = useCallback(e => {
         setValue(e.target.value);
-    }, [])
+    }, []);
+    const onSubmit = useCallback(
+        //submit 해준 이유 - enter에도 작동
+        //onClick으로 작성후 enter감지해도 가능
+        e => {
+            onInsert(value);
+            setValue('');
+            //submit 이벤트는 새로고침 발생 이를 방지
+            e.preventDefault();
+        },
+        [onInsert, value],
+    );
     return (
-        <form className="TodoInsert">
+        <form className="TodoInsert" onSubmit={onSubmit}>
             <input placeholder="할 일을 입력하세요"
                 value={value}
                 onChange={onChange} />
