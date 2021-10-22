b544faa7d9ee163043967ae1dd9bf1e7bd317b99
82102 <sms7624@gmail.com>
2021-10-22 23:56:59 +0900

    refactoring

--- src/modules/sample.js
+++ src/modules/sample.js
@@ -1,5 +1,6 @@
 import { handleActions } from 'redux-actions';
 import * as api from '../lib/api';
+import createRequestThunk from '../lib/createRequestThunk';
 
 const GET_POST = 'sample/GET_POST';
 const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
@@ -9,6 +10,9 @@ const GET_USERS = 'sample/GET_USERS';
 const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
 const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
+export const getPost = createRequestThunk(GET_POST, api.getPost);
+export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
+/*
 export const getPost = id => async dispatch => {
     dispatch({ type: GET_POST });
     try {
@@ -44,6 +48,7 @@ export const getUsers = () => async dispatch => {
         throw (e);
     }
 };
+*/
 
 const initialState = {
     loading: {
