d1cfbf026ce878ab5239f8c8ecb07a7827d224a8
82102 <sms7624@gmail.com>
2021-10-29 17:42:25 +0900

    webpack

--- src/index.server.js
+++ src/index.server.js
@@ -3,4 +3,6 @@ import ReactDOMServer from 'react-dom/server';
 
 const html = ReactDOMServer.renderToString(
     <div>Hello Server Side Rendering</div> //jsx를 넣어서 호출하면 렌더링 결과를 문자열로 반환합니다.
-)
+);
+
+console.log(html);
