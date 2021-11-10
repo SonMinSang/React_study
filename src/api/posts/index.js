f5b5b87c5341826d5b9f1cef6f84ad08210f6923
82102 <sms7624@gmail.com>
2021-11-11 00:38:36 +0900

    refactoring

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -5,9 +5,8 @@ const posts = new Router();
 
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.list);
-posts.get('/:id', postsCtrl.list);
-posts.delete('/:id', postsCtrl.list);
-posts.put('/:id', postsCtrl.list);
-posts.patch('/:id', postsCtrl.list);
+posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.list);
+posts.delete('/:id', postsCtrl.checkObjectId, postsCtrl.list);
+posts.patch('/:id', postsCtrl.checkObjectId, postsCtrl.list);
 
-module.exports = posts;
+export default posts;
